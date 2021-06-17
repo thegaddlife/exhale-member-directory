5146c4105d38b29670ef82e9bf4c6b79f6a38e80
Brett Gadd <brettgadd@gmail.com>
2021-06-17 16:17:30 -0700

    member card

---
+++ interfaces/Member.ts
@@ -0,0 +1,12 @@
+// to import
+// import { Member } from 'path/to/interfaces';
+
+export type Member = {
+  id: number
+  name: string
+  title: string
+  role: string
+  email: string
+  telephone: string
+  imageUrl: string
+}
