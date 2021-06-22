bf55006f2bc7df15f51cb06fff0c03295f086e62
Brett Gadd <brettgadd@gmail.com>
2021-06-21 21:37:42 -0700

    directory page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,48 +1,18 @@
 import Head from 'next/head'
-import MemberGrid from '../components/MembersGrid'
-// import { GetStaticProps } from 'next'
-import { Member } from '../interfaces/Member'
-import getAllMembers from '../lib/members'
+import React from 'react'
+import Link from 'next/link'
 
-type Props = {
-  allMembers: Member[]
-}
-
-const Home = ({ allMembers }: Props) => {
+const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
         <title>Exhale Creativity - Member Directory</title>
         <link rel="alternate icon" type="image/png" href="/favicon.png" />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-          var MemberSpace = window.MemberSpace || {subdomain: 'exhalecreativity', domain: 'members.exhalecreativity.com'};
-          (function(d){
-          var s = d.createElement('script');
-          s.src = 'https://cdn.memberspace.com/scripts/widgets.js';
-          var e = d.getElementsByTagName('script')[0];
-          e.parentNode.insertBefore(s,e);
-          }(document));  
-                `,
-          }}
-        ></script>
       </Head>
-
-      <MemberGrid members={allMembers} />
+      <Link href="/directory">Directory</Link>
     </div>
   )
 }
 
 export default Home
-
-export const getStaticProps = async () => {
-  // Example for including static props in a Next.js function component page.
-  // Don't forget to include the respective types for any props passed into
-  // the component.
-  const allMembers: Member[] = await getAllMembers()
-
-  return { props: { allMembers } }
-}
